75a5ff1c5142c1327b5d737b44bdb16bdb17389a
Serge Voytenko <svoytenko@jamis.com>
2023-03-01 13:12:30 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace interview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
